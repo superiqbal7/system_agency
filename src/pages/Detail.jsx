805a1e8b1f4a39284a1c1639f4b72df822bd1547
Rahim Iqbal <superiqu@gmail.com>
2021-01-04 02:26:23 +0600

    fix padding

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -77,57 +77,58 @@ class Detail extends Component {
 			<div>
 				<Navigation />
 				<section className="mt-6 pt-32">
-					<div className="row mt-16">
-
 						{data.map((talent, i) => (
-							<div>
-								<div className="row">
-									<div className="col-sm-4">
-										<div className="pleft">
-											<h3 className="detailTalentLastname"><strong>{talent.name} </strong> {talent.last_name} </h3>
-											<table className="tableTalentDetails helvetica-neue-light" style={{ width: '100%', marginTop: '7rem', margin: '70px 15px 40px' }}>
-												<tr>
-													<th> HEIGHT </th>
-													<td className="helvetica-neue-bold">{talent.height}</td>
-												</tr>
-												<tr>
-													<th> BUST</th>
-													<td className="helvetica-neue-bold">{talent.bust}</td>
-												</tr>
-												<tr>
-													<th> WAIST</th>
-													<td className="helvetica-neue-bold">{talent.waist}</td>
-												</tr>
-												<tr>
-													<th> HIPS</th>
-													<td className="helvetica-neue-bold">{talent.hips}</td>
-												</tr>
-												<tr>
-													<th> SHOES</th>
+							<div className="pt-32">
+								<div className="row mx-0">
+									{this.state.type == 'talent_video' ? ' ' : (
+										<div className="col-sm-4 ">
+											<div className="pleft">
+												<h3 className="detailTalentLastname"><strong>{talent.name} </strong> {talent.last_name} </h3>
+												<table className="tableTalentDetails helvetica-neue-light" style={{ width: '100%', marginTop: '7rem', margin: '70px 15px 40px' }}>
+													<tr>
+														<th> HEIGHT </th>
+														<td className="helvetica-neue-bold">{talent.height}</td>
+													</tr>
+													<tr>
+														<th> BUST</th>
+														<td className="helvetica-neue-bold">{talent.bust}</td>
+													</tr>
+													<tr>
+														<th> WAIST</th>
+														<td className="helvetica-neue-bold">{talent.waist}</td>
+													</tr>
+													<tr>
+														<th> HIPS</th>
+														<td className="helvetica-neue-bold">{talent.hips}</td>
+													</tr>
+													<tr>
+														<th> SHOES</th>
 
-													<td className="helvetica-neue-bold">{talent.shoes}</td>
-												</tr>
-												<tr>
-													<th> EYES</th>
+														<td className="helvetica-neue-bold">{talent.shoes}</td>
+													</tr>
+													<tr>
+														<th> EYES</th>
 
-													<td className="helvetica-neue-bold">{talent.eyes}</td>
-												</tr>
-												<tr>
-													<th> HAIR</th>
-													<td className="helvetica-neue-bold">{talent.hair}</td>
-												</tr>
-											</table>
-											<div className="pdf_btn">
-												<i class="fa fa-instagram" style={{ fontsize: "24px", display: "block" }}></i>
-												<a className="helvetica-neue-italic" href={"https://www.instagram.com/" + formatInstagramContext(talent.instagram)}>@{formatInstagramContext(talent.instagram)}</a>
-												<div>
-													<section className="flex justify-center mt-5 helvetica-neue-thin italic about-office-buttons" >
-														<button className=" hover:font-extrabold"> <a target="_blank" href={`${config.URL}${talent.pdf_route}`} style={{ color: 'black', fontSize: '12px' }}>create pdf</a></button>
-													</section>
+														<td className="helvetica-neue-bold">{talent.eyes}</td>
+													</tr>
+													<tr>
+														<th> HAIR</th>
+														<td className="helvetica-neue-bold">{talent.hair}</td>
+													</tr>
+												</table>
+												<div className="pdf_btn">
+													<i class="fa fa-instagram" style={{ fontsize: "24px", display: "block" }}></i>
+													<a className="helvetica-neue-italic" href={"https://www.instagram.com/" + formatInstagramContext(talent.instagram)}>@{formatInstagramContext(talent.instagram)}</a>
+													<div>
+														<section className="flex justify-center mt-5 helvetica-neue-thin italic about-office-buttons" >
+															<button className=" hover:font-extrabold"> <a target="_blank" href={`${config.URL}${talent.pdf_route}`} style={{ color: 'black', fontSize: '12px' }}>create pdf</a></button>
+														</section>
+													</div>
 												</div>
 											</div>
 										</div>
-									</div>
+									) }
+									
 									{
 										this.state.renderSlider && this.state.sliderItems.length > 0 ?
 											null
@@ -137,7 +138,7 @@ class Detail extends Component {
 											</div>
 									}
 
-									<div className="col-sm-8">
+									<div className={this.state.type == 'talent_video' ? "col-sm-12" :  "col-sm-8"}>
 										{
 											this.state.renderSlider && this.state.sliderItems.length > 0 ?
 												<div>
@@ -146,19 +147,18 @@ class Detail extends Component {
 												:
 												null
 										}
-										<div className="detail_btn" style={{ marginLeft: '100px', marginTop: '50px', fontStyle: 'italic' }}>
-											<label className={this.state.type == 'talent_portfolio' ? 'active' : ''} onClick={() => this.silderShow("talent_portfolio")}>PORTFOLIO</label>
-											<label className={this.state.type == 'talent_polaroid' ? 'active' : ''} onClick={() => this.silderShow("talent_polaroid")}>POLAS</label>
-											<label class={this.state.type == 'talent_video' ? 'active' : ''} onClick={() => this.silderShow("talent_video")}> VIDEOS</label>
-											{/* <button> <Link>EDIT</Link></button> */}
-										</div>
 									</div>
+								</div>
+
+								<div className="detail_btn text-center my-40" style={{ fontStyle: 'italic' }}>
+									<label className={this.state.type == 'talent_portfolio' ? 'active px-12' : 'px-12'} onClick={() => this.silderShow("talent_portfolio")}>PORTFOLIO</label>
+									<label className={this.state.type == 'talent_polaroid' ? 'active px-12' : 'px-12'} onClick={() => this.silderShow("talent_polaroid")}>POLAS</label>
+									<label class={this.state.type == 'talent_video' ? 'active px-12' : 'px-12'} onClick={() => this.silderShow("talent_video")}> VIDEOS</label>
+									{/* <button> <Link>EDIT</Link></button> */}
 								</div>
 
 							</div>
 						))}
-
-					</div>
 				</section>
 				<Footer />
 			</div>
